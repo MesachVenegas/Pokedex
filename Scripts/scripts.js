651857d31b6aa4691d9c8ac867736d8679d19762
Mesach Venegas <99062079+MesachVenegas@users.noreply.github.com>
2022-03-28 04:13:39 -0700

    add: code para cambio de icono de tipos

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -38,11 +38,15 @@ const load_data = (data) => {
     const alt = document.getElementById("altura");
     name.textContent = `${name_pokemon.charAt(0).toUpperCase() + name_pokemon.slice(1)}`;
     id.textContent = `${id_pkm}`;
-    alt.textContent = `${altura}`;
+    alt.textContent = `${altura.toString()}0 cm`;
     // console.log(habilidades)
 
     // Carga de los tipos de pokemon
     let tipos = [];
+    let icon_type =  document.getElementById('icon_type');
+    let icon_type2 =  document.getElementById('icon_type2');
+    console.log(icon_type)
+    console.log(icon_type2)
     for (const property in type_pkm){
         // console.log(property)
         let val_property = type_pkm[property];
@@ -51,13 +55,80 @@ const load_data = (data) => {
             var tipo = val_property[value];
         }
         tipos.push(tipo.name);
-        // Representacion de info de estados del pokemon según sus tipos.
-        if(tipos.length > 1){
-            type.textContent= `${tipos[0].charAt(0).toUpperCase() + tipos[0].slice(1)} / ${tipos[1].charAt(0).toUpperCase() + tipos[1].slice(1)}`;
+        // Muestra de el tipo al que pertenece el pokemon.
+        icon_type.width = "55";
+        icon_type.alt = tipos[0]
+        icon_type2.width = "55";
+        icon_type2.alt = tipos[1]
+        console.log(tipos[0])
+        console.log(tipos[1])
+
+        switch(tipos[0]){
+            case "fire":
+                icon_type.src = "./Img/Tipos/fuego.png";
+                break;
+            case "water":
+                icon_type.src = "./Img/Tipos/agua.png";
+                break;
+            case "poison":
+                icon_type.src = "./Img/Tipos/veneno.png";
+                break;
+            case "grass":
+                icon_type.src = "./Img/Tipos/planta.png";
+                break;
+            case "psychic":
+                icon_type.src = "./Img/Tipos/psiquico.png";
+                break;
+            case "rock":
+                icon_type.src = "./Img/Tipos/roca.png";
+                break;
+            case "ground":
+                icon_type.src = "./Img/Tipos/tierra.png";
+                break;
+            default:
+                icon_type.width = "30";
+                icon_type.width = "./Img/pokeball_type.png";
+                break;
+        }
+        if(tipos[1]){
+            icon_type2.style = "visibility: visible"
+            switch(tipos[1]){
+                case "fire":
+                    icon_type2.src = "./Img/Tipos/fuego.png";
+                    break;
+                case "water":
+                    icon_type2.src = "./Img/Tipos/agua.png";
+                    break;
+                case "poison":
+                    icon_type2.src = "./Img/Tipos/veneno.png";
+                    break;
+                case "grass":
+                    icon_type2.src = "./Img/Tipos/planta.png";
+                    break;
+                case "psychic":
+                    icon_type2.src = "./Img/Tipos/psiquico.png";
+                    break;
+                case "rock":
+                    icon_type2.src = "./Img/Tipos/roca.png";
+                    break;
+                case "ground":
+                    icon_type2.src = "./Img/Tipos/tierra.png";
+                    break;
+                default:
+                    icon_type2.width = "30";
+                    icon_type2.width = "./Img/pokeball_type.png";
+                    break;
+            }
         }
         else{
-            type.textContent= tipos;
-        }
+                icon_type2.style = "visibility: hidden"
+            }
+        // if(tipos.length > 1){
+        //     type.textContent= `${tipos[0].charAt(0).toUpperCase() + tipos[0].slice(1)} / ${tipos[1].charAt(0).toUpperCase() + tipos[1].slice(1)}`;
+        // }
+        // else{
+        //     type.textContent= `${tipos[0].charAt(0).toUpperCase() + tipos[0].slice(1)}`;
+        // }
     }
 }
 
@@ -91,14 +162,8 @@ const bar_fill = (data) => {
     let bar_es_atk = document.getElementById('bar_es_atk');
     let bar_es_def = document.getElementById('bar_es_def');
     let bar_speed = document.getElementById('bar_speed');
-    if(bar_hp.classList[1] == true){
-        console.log(bar_hp.classList[1])
-        bar_hp.classList[1] = `skill-fill-${calcular_porcentaje(hp_value)}`;
-    }
-    else{
-        // console.log(bar_hp.classList)
-        bar_hp.classList.add(`skill-fill-${calcular_porcentaje(hp_value)}`)
-    }
+
+    bar_hp.classList.add(`skill-fill-${calcular_porcentaje(hp_value)}`)
     bar_atk.classList.add(`skill-fill-${calcular_porcentaje(atk_value)}`)
     bar_def.classList.add(`skill-fill-${calcular_porcentaje(def_value)}`)
     bar_es_atk.classList.add(`skill-fill-${calcular_porcentaje(es_atk_value)}`)
